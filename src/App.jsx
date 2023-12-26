34115709dee86ecc0561ffee5d0b2d822373d2d0
BakuEmilov <emilovbaku530@gmail.com>
2023-12-26 16:42:19 +0600

    opened the home, login. register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
 function App() {
-  return <></>;
+  return (
+    <Switch>
+      <Route exact path="/" Component={HomePage} />
+      <Route exact path="/login" Component={LoginPage} />
+      <Route exact path="/register" Component={RegisterPage} />
+    </Switch>
+  );
 }
 
 export default App;
