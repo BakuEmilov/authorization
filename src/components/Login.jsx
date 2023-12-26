46c979d8adb05ebcac7c9780af5cebaf56cc437c
BakuEmilov <emilovbaku530@gmail.com>
2023-12-26 21:11:12 +0600

    during the login process

---
+++ src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useDispatch } from 'react-redux';
+import { setUser } from '../store/slices/userSlice'
+
+const Login = () => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+
+  }
+
+  return (
+    <div></div>
+  )
+}
+
+export {Login}
