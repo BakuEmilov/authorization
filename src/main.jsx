9f3935bb6523e56ce24a164636ca8728f0b27731
BakuEmilov <emilovbaku530@gmail.com>
2023-12-26 17:44:06 +0600

    set up firebase

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { store } from "./store/index.js";
+import './firebase'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
