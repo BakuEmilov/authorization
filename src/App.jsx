c54409050fd625197d9845775b32ce03f0e1e2e4
BakuEmilov <emilovbaku530@gmail.com>
2023-12-26 17:08:44 +0600

    I changed the Switch to Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { Switch, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <Switch>
+    <Routes>
       <Route exact path="/" Component={HomePage} />
       <Route exact path="/login" Component={LoginPage} />
       <Route exact path="/register" Component={RegisterPage} />
-    </Switch>
+    </Routes>
   );
 }
 
